7347950bb1a8c5756292fa1a5e79a1eb9292e477
ThatcheRRR <pavelosipov777@mail.ru>
2019-11-26 11:31:35 +0300

    feat: add animation to scrolling and showing description

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     hide();
-    // flight.style.display = 'none';
+    flight.style.display = 'none';
+    footer.classList.add('hidden');
 }
 
 class User {
@@ -24,8 +25,6 @@ class Flight {
 
 }
 
-const dateField = document.querySelector('#departureDate');
-
 let date = new Date();
 let hours = 0;
 let minutes = 0;
@@ -41,6 +40,7 @@ if (date.getMinutes() < 10) {
     minutes = date.getMinutes();
 }
 
+const dateField = document.querySelector('#departureDate');
 const currentDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + 'T' + hours + ':' + minutes;
 const loginButton = document.querySelector('.loginBut');
 const authBlock = document.querySelector('.auth__block');
@@ -48,15 +48,23 @@ const cabinet = document.querySelector('.current-user');
 const container = document.querySelector('.container');
 const flight = document.querySelector('.flight');
 const findTickets = document.querySelector('.button-block');
+const departurePlace = document.querySelector('#departurePlace');
+const arrivalPlace = document.querySelector('#arrivalPlace');
+const footer = document.querySelector('footer');
 const enterCode = 13;
+const user = {};
+const travel = {};
 
 dateField.min = currentDate;
 dateField.value = currentDate;
 
 let userName = '';
 let currentID = 0;
+let property = '';
+let count = 0;
 
 flight.style.height = `${document.body.offsetHeight}px`;
+
 container.style.height = `${document.body.offsetHeight}px`;
 
 function opacity() {
@@ -78,19 +86,72 @@ function loggedIn(e) {
         authBlock.style.opacity = '0';
         setTimeout(hide, 300);
         cabinet.textContent = userName[0].toUpperCase() + userName.substring(1).toLowerCase();
-        return user = new User(userName, 20, 4);
     }
 }
 
 authBlock.addEventListener('keydown', loggedIn);
+
 loginButton.addEventListener('click', () => {
     userName = document.querySelector('#name').value.toLowerCase();
     authBlock.style.opacity = '0';
     setTimeout(hide, 300);
     cabinet.textContent = userName[0].toUpperCase() + userName.substring(1).toLowerCase();
-    return user = new User(userName, 20, 4);
 });
 
 findTickets.addEventListener('click', () => {
     flight.style.display = 'flex';
+    section = '';
+    const addTicket = document.querySelector(".purchase__amount_plus");
+    const removeTicket = document.querySelector(".purchase__amount_minus");
+    const showButton = document.querySelector(".tickets__buy_button > svg");
+    const buySeats = document.querySelector(".purchase__amount_current-amount");
+    const departureCity = document.querySelector('.departure__city');
+    const arrivalCity = document.querySelector('.arrival__city');
+    const purchase = document.querySelector('.purchase__type');
+    const buyTicket = document.querySelectorAll('.tickets__buy_price');
+    const economAmount = document.querySelector('.availableSeats__classes_econom');
+    const businessAmount = document.querySelector('.availableSeats__classes_business');
+    let economAll = +document.querySelector('.availableSeats__classes_econom > span').textContent;
+    let businessAll = +document.querySelector('.availableSeats__classes_business > span').textContent;
+    count = Number(buySeats.innerText);
+    property = document.querySelector('.tickets__buy_property');
+    property.classList.add('hidden');
+    // let arrival_city = arrivalPlace.value.toLowerCase();
+    // let departure_city = departurePlace.value.toLowerCase();
+    // arrivalCity.textContent = arrival_city[0].toUpperCase() + arrival_city.substring(1).toLowerCase();
+    // departureCity.textContent = departure_city[0].toUpperCase() + departure_city.substring(1).toLowerCase();
+    showButton.addEventListener('click', showDescrInfo);
+    footer.classList.remove('hidden');
+    purchase.children[1].addEventListener('click', (e) => {
+        if (e.currentTarget.innerText === 'Эконом') {
+            alert('kok');
+        } else if (e.currentTarget.innerText === 'Бизнес') {
+            alert('kek');
+        }
+    });
+    addTicket.onclick = () => {
+        if (count >= 9) {
+            buySeats.innerText = '9';
+        } else {
+            buySeats.innerText = `${++count}`;
+        }
+    }
+    removeTicket.onclick = () => {
+        if (count <= 1) {
+            buySeats.innerText = '1';
+        } else {
+            buySeats.innerText = `${--count}`;
+        }
+    }
 });
+
+function showDescrInfo(e) {
+    let elem = getComputedStyle(e.currentTarget);
+    let check = elem.transform;
+    if (check !== 'none') {
+        e.currentTarget.style.transform = '';
+    } else {
+        e.currentTarget.style.transform = 'rotate(180deg)';
+    }
+    property.classList.toggle('hidden');
+}
